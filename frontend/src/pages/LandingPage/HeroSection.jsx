bb50339b37755e9ae64bc92931751e468b089458
krati-porwal <porwalkrati095@gmail.com>
2025-07-12 14:25:07 +0530

    Task krati : add reusable form component

--- frontend/src/pages/LandingPage/HeroSection.jsx
+++ frontend/src/pages/LandingPage/HeroSection.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "../../components/Header";
 import Pagination from "../../components/pagination";
 import SkillCard from "../../components/SkillCard";
+import UserProfileForm from "../../components/Form";
 
 const HeroSection = () => {
    
@@ -12,6 +13,10 @@ const HeroSection = () => {
     setCurrentPage(page);
     // You can add logic to fetch new data here
   }
+    //form
+    const handleSave = (data) => {
+    console.log("Submitted Data:", data);
+  };
   return (
     <>
       <Header />
@@ -29,6 +34,16 @@ const HeroSection = () => {
         totalPages={totalPages}
         onPageChange={handlePageChange}
       />
+
+       <section className="row mx-0 py-5 justify-content-center">
+      <div className="col-md-8 col-12 px-4">
+        <div className="d-flex justify-content-between align-items-center mb-4">
+          <h3>User Profile</h3>
+          <img src="/avatar.png" alt="Profile" style={{ width: 50, borderRadius: "50%" }} />
+        </div>
+        <UserProfileForm onSubmit={handleSave} />
+      </div>
+    </section>
     </>
   );
 };
