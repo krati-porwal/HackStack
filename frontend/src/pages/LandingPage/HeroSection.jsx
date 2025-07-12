4d56f0f0d778ee306f38940d9a00b63d5613e9a2
krati-porwal <porwalkrati095@gmail.com>
2025-07-12 13:46:48 +0530

    Task krati : add reusable skill card component

--- frontend/src/pages/LandingPage/HeroSection.jsx
+++ frontend/src/pages/LandingPage/HeroSection.jsx
@@ -1,9 +1,19 @@
 import Header from "../../components/Header"
+import SkillCard from "../../components/SkillCard"
 
 const HeroSection = () =>{
     return(
         <>
         <Header/>
+       <SkillCard
+  name="Krati"
+  profilePhoto="https://example.com/krati.jpg"
+  skillsOffered={['React', 'Python']}
+  skillsWanted={['DevOps', 'System Design']}
+  rating={4.8}
+  isLoggedIn={true}
+  onRequest={() => alert('Requested')}
+/>
 
         </>
     )
